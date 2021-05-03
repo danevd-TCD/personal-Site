d6c3cfa89a8f7deaf82cb31b48c58865668fdc32
Daniel (Remote) <danevd@tcd.ie>
2021-05-03 14:57:20 +0000

    updated flask

--- siteFile.py
+++ siteFile.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def root():
-        return "<h1> Flask dev test v1.0.2 </h1>"
+        return render_template('index.html')
 
 if __name__ == "__main__":
         app.run()
