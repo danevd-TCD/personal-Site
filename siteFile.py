53c1ce9a01c9d4185364733a4542a3bb080e069c
Daniel Danev <danevd@tcd.ie>
2021-05-19 18:40:01 +0100

    changed image files, big site aesthetic redesign

--- siteFile.py
+++ siteFile.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file, url_for
+import os
 
 app = Flask(__name__, static_url_path='')
+
+imgArray = None
+def getImages():
+        global imgArray
+        imgArray = []
+        for fileName in os.listdir(app.static_folder + '/gallery'):
+                imgArray.append(os.path.join('static/gallery/', fileName))
 
 @app.route('/')
 def root():
         return render_template('index.html')
 
+#endpoint for gallery images; send file if present, 404 if not
+@app.route('/static/gallery/<image>')
+def returnImage(image):
+        return send_file(app.static_folder + '/gallery/' + image)
+
 @app.route('/gallery')
 def gallery():
-        return render_template('gallery.html')
+        getImages()
+        return render_template('gallery.html', imgList = imgArray)
+        #return "<p>" + str(imgArray) + "</p>"
 
 if __name__ == "__main__":
-        app.run()
+        app.run(debug=True)
