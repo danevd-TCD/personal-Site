f743d87036fc2facd76d3c107a07c6e0812c9470
Daniel Danev <danevd@tcd.ie>
2021-05-26 13:19:03 +0100

    initial API endpoint

--- siteFile.py
+++ siteFile.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_file, url_for
+from flask import Flask, render_template, send_file, url_for, jsonify
 import os
 
 app = Flask(__name__, static_url_path='')
@@ -28,6 +28,15 @@ def gallery():
         getImages()
         return render_template('gallery.html', imgList = imgArray)
         #return "<p>" + str(imgArray) + "</p>"
+
+#initial hardcoded API response for debug/test
+status = {'isUp':'Yes','memUse':750,'cpuUse':'15%','upSince':1621768538}
+
+#basic API endpoint
+@app.route('/status/all', methods=['GET'])
+def status_all():
+        return(jsonify(status)) #jsonify isn't strictly necessary anymore
+
 
 if __name__ == "__main__":
         app.run(debug=True)
